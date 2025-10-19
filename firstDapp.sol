d40f76cca3d2278ec9b91393105eff1cf6c9acca
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-10-19 13:58:47 -0400

    Init - syntax basics

---
+++ firstDapp.sol
@@ -0,0 +1,12 @@
+// import solidity
+pragma solidity >=0.8.2 <0.9.0;
+
+// importing from other files
+import "filename1";
+import * as symbolname from "filename2";
+import {symb1 as alies, symbol2} from "filename3";
+
+// your firt contract
+contract SimpleContract {
+    // contract code
+}
