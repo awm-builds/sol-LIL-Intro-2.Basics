ac9130b4095baee37a01dd6e51dbdf79d20137db
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-10-23 18:53:43 -0400

    Added operators and conditionals branches. More in OneNotes.

--- firstDapp.sol
+++ firstDapp.sol
@@ -1,9 +1,17 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract SimpleContract {
-    function calcs(uint _a, uint _b) public pure
-    returns (uint o_sum, uint o_product) {
-        o_sum = _a + _b;
-        o_product = _a * _b;
-    }
+    // Arithmic operators = + - * / % ++ --
+    uint a = 2;
+    uint b = 3;
+
+    uint c = a + b;
+
+    // logical operators
+    // ! && || == !=
+    bool hasMoney = !false;
+
+    // Bitwise operators
+    // & | ^ ~ << >>
+    bytes1 contractValue = 0x02 | 0x01;
 }
