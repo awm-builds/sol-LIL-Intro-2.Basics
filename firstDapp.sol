ffadf712d35c21d120a0ff3df95e9f20606a644e
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-10-23 17:59:47 -0400

    Added functions branch. The example shows a function returning 2 outputs based on 2 calculated inputs.

--- firstDapp.sol
+++ firstDapp.sol
@@ -1,19 +1,9 @@
 pragma solidity >=0.8.2 <0.9.0;
 
 contract SimpleContract {
-    // block
-    block.number;
-    block.difficulty;
-    block.coinbase ();
-
-    /* multiple lin comments */
-
-    // message
-    msg.sender;
-    msg.data;
-    msg.value;
-
-    // transaction
-    tx.origin;
-
+    function calcs(uint _a, uint _b) public pure
+    returns (uint o_sum, uint o_product) {
+        o_sum = _a + _b;
+        o_product = _a * _b;
+    }
 }
