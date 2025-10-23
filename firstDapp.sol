32672a09eb16b559f6ce4ab038bf6e2552ac0d89
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-10-23 17:45:49 -0400

    Added special variables branch. Applying a dot after those keywords pulls up dropdown suggestions like a class.

--- firstDapp.sol
+++ firstDapp.sol
@@ -1,21 +1,19 @@
-// import solidity
 pragma solidity >=0.8.2 <0.9.0;
 
-// importing from other files
-// import "filename1";
-// import * as symbolname from "filename2";
-// import {symb1 as alies, symbol2} from "filename3";
-
-// your firt contract
 contract SimpleContract {
-// ether gwei wei
-bool isEqual = (1 ether == 1e18 wei);
+    // block
+    block.number;
+    block.difficulty;
+    block.coinbase ();
 
-/* seconds, mins, hrs, days wks, yrs 
-*Are all reserved words* */
-// time units
-bool isTime = (1 hours == 60 minutes);
+    /* multiple lin comments */
 
-// 
+    // message
+    msg.sender;
+    msg.data;
+    msg.value;
+
+    // transaction
+    tx.origin;
 
 }
